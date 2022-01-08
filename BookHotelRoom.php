9fff45c7855f3e64dcc71ec249973066b6acd87f
Phuong Thao <phuongthao2104.na@gmail.com>
2022-01-08 12:07:54 +0700

    commit

---
+++ BookHotelRoom.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Đặt phòng</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <header>
+    <?php include "./Header.php" ?>
+    </header>
+    <main class = 'main bg-light' >
+        <button>Quay lại</button>
+    <h1>Thông tin đặt phòng</h1>
+    <div class="container-sm bg-light">
+    <div class="row">
+        <div class="left col-sm-8">
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat similique quidem facilis iste, optio enim, molestiae nostrum ratione excepturi ut sapiente dicta iure consequuntur omnis ipsa repellat! Nam, sed repellat!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam qui tempore, maiores ut quae doloremque ipsam delectus consequatur? Cupiditate ratione voluptates nisi eligendi optio unde suscipit quasi, deleniti odit molestias.
+        </div>
+        <div class="right col-sm-4 mt-4">
+              fhdguysdfguyf
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod similique deleniti labore ex excepturi omnis impedit, facilis quidem blanditiis sequi officia, inventore dolorem. Esse voluptatibus sit architecto, expedita corrupti numquam.
+        </div>
+    </div>
+    </main>
+</body>
+</html>
